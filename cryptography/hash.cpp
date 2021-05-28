d127948ae1901384a23d845b12f8f898332faf9b
Gateway47 <itsregalo047@gmail.com>
2021-05-28 20:34:02 +0300

    hash

---
+++ cryptography/hash.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,x;
+    char str[75]
+
+    cout<<"Please enter key:\t";
+    cin>>str;
+
+    cout <<"\nplease  ose folowing options:\n";
+    cout <<"1 = encryption";
+    cout <<"2 = decryption";
+    cin>>x;
+
+    switch(x){
+    case 1:
+        for(i=0; (i<75&& str[i
+
+    }
+    return 0;
+}
