b442548343d250acab10e8e452069f71db3c163c
Gateway47 <itsregalo047@gmail.com>
2021-06-07 21:16:29 +0300

    for lop power

---
+++ examples/forloopPower.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int num1, num2, answer;
+
+    cout<<"Enter the numbers\n";
+    cin>>num1>>num2;
+
+    for(n=num2, n>0, num2--){
+        answer=num1*num1;
+    }
+}
