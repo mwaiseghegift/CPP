071cba99e8ee4aa92d01b55bfaffdbf8395eebc5
Gateway47 <itsregalo047@gmail.com>
2021-06-12 21:30:38 +0300

    while instead of nested if

--- examples/calcWithValidation.cpp
+++ examples/calcWithValidation.cpp
@@ -29,34 +29,17 @@ int main(){
         cin.clear();
         
         cout<<"Enter the number again\n";
-        
+        cin>>num1;
     }
 
     cout<<"Enter The second Number"<<endl;
     cin>>num2;
 
-    if (num2 == NULL){
-        cout<<"Please enter the value"<<endl;
-        cin>>num2;
-    }
 
     cout<<"Choose an operator\n1 for +\n2 for -\n3 for *\n4 for /\n";
     cin>>choiceOperator;
-    if (choiceOperator==NULL){
-        cout<<"Please enter an operator"<<endl;
-        cin>>choiceOperator;
-    }
 
-    if (choiceOperator==1){
-        cout<<addition(num1,num2);
-    }else if (choiceOperator==2){
-        cout<<subtract(num1,num2);
-    }else if (choiceOperator==3){
-        cout<<multiply(num1, num2);
-    }else if (choiceOperator==4){
-        cout<<divide(num1, num2);
-    }else
-        cout<<"You entered an invalis operator"<<endl;
+    
 
     return 0;
 
