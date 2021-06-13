1a7a8c8b71baab9bf2bf023c14a4707992b887b2
Gateway47 <itsregalo047@gmail.com>
2021-06-13 17:43:33 +0300

    try out nested

--- validation/numericValidation.cpp
+++ validation/numericValidation.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
-#include <cctype>
 using namespace std;
+
+int main(){
+    int choice;
+
+    cout<<"Enter Choice\n";
+    cin>>choice;
+
+    while(choice>0){
+        switch(choice){
+            case 1:
+                cout<<"You chose 1\n";
+                break;
+            case 2:
+                cout<<"You chose 2\n";
+                break;
+            case 3:
+                cout<<"You chose 3\n";
+                break;
+            default:
+                cout<<"Wrong input...Try again";
+        }
+        
+        cout<<"Enter Choice: ";
+        cin>>choice;
+    }
+    return 0;
+}
