86f5d504deaf1a7ae7da454aa6a48eceb04cd0c8
Gateway47 <itsregalo047@gmail.com>
2021-06-16 19:44:17 +0300

    while correct syntax

---
+++ examples/correctWhileLoop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/* 
+int n=10;
+while (n>0)
+n/=2
+cout<<n*n<<endl;
+ */
+
+//correct syntax
+int main(){
+    int n=10;
+    while (n>0){
+        n/=2;
+        cout<<n*n<<endl;
+    }
+    return 0;
+}
