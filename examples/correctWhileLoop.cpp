88661340688f58dcea5049d567a2431d194ae65c
Gateway47 <itsregalo047@gmail.com>
2021-06-16 19:53:57 +0300

    while corrected

--- examples/correctWhileLoop.cpp
+++ examples/correctWhileLoop.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 using namespace std;
 
-/* 
-int n=10;
-while (n>0)
-n/=2
-cout<<n*n<<endl;
- */
 
 //correct syntax
 int main(){
     int n=10;
     while (n>0){
-        n/=2;
+        n/=2; 
+        //same as n=n/2
         cout<<n*n<<endl;
     }
     return 0;
